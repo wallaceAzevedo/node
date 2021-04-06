6cf89dd288fa654c118bde7731a7a48bdbbbd86e
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-04-06 18:40:16 -0300

    segundo submit

--- api.js
+++ api.js
@@ -25,9 +25,9 @@ app.get('/', function(req, res){
     res.send(getHello());
 });
 
-app.get('/users', function(req, res){
-    res.send(getUsers());
-});
+const userRouter = require('./src/routes/user-route');
+
+app.use('/user', userRoute);
 
 app.listen(4000, function(){
     console.log('Hello Express listen on port 4000');
---
+++ src/controller/user-controller.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var USERS =[
+    {'id': 1, 'username': 'wallaceazevedo', 'password': '123456'},
+    {'id': 2, 'username': 'paul', 'password': '123456'},
+];
+
+function getUsers(){
+    return USERS;
+}
+
+exports.get = async(req, res, next) =>{
+    console.log('Entrou no nosso controller');
+    res.send(getUsers());
+};
